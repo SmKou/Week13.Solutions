6bf64350ec2b2212219e6eee1ef6f3228e68c248
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-13 17:06:23 -0700

    WIP Add Newtonsoft.Json

--- ApiCall/ConsoleApiCall/Program.cs
+++ ApiCall/ConsoleApiCall/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ConsoleApiCall.Keys;
+
+namespace ConsoleApiCall;
+
+public class Program
+{
+    public static void Main()
+    {
+        Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
+        string result = apiCallTask.Result;
+        JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+        Console.WriteLine(jsonResponse["results"]);
+    }
+}
+
+public class ApiHelper
+{
+    public static async Task<string> ApiCall(string apiKey)
+    {
+        RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+        RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.Get);
+        RestResponse response = await client.ExecuteAsync(request);
+        return response.Content;
+    }
+}
