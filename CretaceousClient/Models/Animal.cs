33e7c78041e304f9a374f870523cf0b9b61c30b6
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-13 23:40:04 -0700

    Working client app for CRUD

--- CretaceousClient/Models/Animal.cs
+++ CretaceousClient/Models/Animal.cs
@@ -21,4 +21,31 @@ public class Animal
         List<Animal> animalList = JsonConvert.DeserializeObject<List<Animal>>(jsonResponse.ToString());
         return animalList;
     }
+
+    public static Animal GetDetails(int id)
+    {
+        var apiCallTask = ApiHelper.Get(id);
+        var result = apiCallTask.Result;
+
+        JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+        Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
+        return animal;
+    }
+
+    public static void Post(Animal animal)
+    {
+        string jsonAnimal = JsonConvert.SerializeObject(animal);
+        ApiHelper.Post(jsonAnimal);
+    }
+
+    public static void Put(Animal animal)
+    {
+        string jsonAnimal = JsonConvert.SerializeObject(animal);
+        ApiHelper.Put(animal.AnimalId, jsonAnimal);
+    }
+
+    public static void Delete(int id)
+    {
+        ApiHelper.Delete(id);
+    }
 }
